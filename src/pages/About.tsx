69adcad5e9f15958f14de6d1bb4904ddded7d050
etah1 <familyta311@gmail.com>
2026-01-09 22:33:53 -0500

    about us mostly done i think

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,46 @@
+import logo from "../assets/logo.png";
+import "./about.css";
+import instagramIcon from "../assets/Instagram_icon.png";
+
 const About = () => {
-    return (
-        <div>
-            <h1>About Us</h1>
-            <p>Welcome to our application! We are dedicated to providing the best service possible.</p>
+  return (
+    <div className="about-page">
+      <div className="about-layout">
+        <img
+          src={logo}
+          alt="About Recovery Recipes"
+          className="about-image"
+        />
+
+        <div className="about-text">
+          <h2>About Us</h2>
+          <p>
+            Recovery Recipes, an on-campus organization whose mission is to
+            combat food insecurity in patients who have recently been
+            discharged from the hospital. The few days after discharge, people
+            may not have access to balanced meals. Additionally, food-insecure
+            patients encounter delayed healing, higher readmission rates, and
+            poorer health outcomes.
+          </p>
+
+          <a
+            href="https://www.instagram.com/recoveryrecipesumd"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="about-instagram"
+            >
+                <img
+                src={instagramIcon}
+                alt="Instagram"
+                className="instagram-icon"
+            />
+            Follow us on Instagram!
+            </a>
+
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default About;
