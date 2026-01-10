64137df1ec634e7109f863ad6e35b17501d17352
etah1 <familyta311@gmail.com>
2026-01-09 23:18:55 -0500

    random things

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -23,7 +23,17 @@ const Home = () =>{
 </p>
         </div>
         <div className="footer-section">
+            <hr className="contact-divider" />
+                <div className="contact-info">
+                    <p><strong>Recovery Recipes</strong></p>
+                    <p>College Park, MD</p>
+                    <p>Email: recoveryrecipes@gmail.com</p>
+                    <p>Phone: 443-546-884</p>
+                </div>
+            <hr className="contact-divider" />
+
             <p>© 2025 Recovery Recipes. All rights reserved.</p>
+            <p> Created by Ethan Ta and Averie Ahn</p>
         </div>
         </div> 
     );
