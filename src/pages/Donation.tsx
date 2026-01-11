02164598560860b872a1ba7d0e4c240b05e87346
Averie Ahn <averieahn@gmail.com>
2026-01-10 23:12:16 -0500

    donation basic

--- src/pages/Donation.tsx
+++ src/pages/Donation.tsx
@@ -1,6 +1,10 @@
 const Donation = () => {
     return (
-        <div>
+        <div donation-page className="donation-page">
+            <div>
+                <img src="../assets/donation.png" alt="Donation Image" className="donation-image"/>
+
+            </div>
             <h1>Donation Page</h1>
             <p>Your support helps us continue our mission. Thank you for considering a donation!</p>
         </div>
