00971db56bf3fb234fd98ffab1f67815ec126a97
etah1 <familyta311@gmail.com>
2026-02-17 18:30:38 -0500

    navBar auto to top

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css'
 import Navbar from './components/Navbar'
 import { Outlet } from 'react-router-dom'
 import Footer from './components/Footer'
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
 
   return (
     <>
     <div className="App">
+      <ScrollToTop />
       <Navbar />
       <Outlet />
       <Footer/ >
